4a46395ddc4517959c87e2eaa73d12ce1bf813e6
Martin Unkel <m-unkel@users.noreply.github.com>
2025-04-29 10:34:07 +0200

    use non-admin symlink

--- src/nvm.go
+++ src/nvm.go
@@ -1194,7 +1194,7 @@ func use(version string, cpuarch string, reload ...bool) {
 		// Create new symlink
 		var ok bool
 		// ok, err = runElevated(fmt.Sprintf(`"%s" cmd /C mklink /D "%s" "%s"`, filepath.Join(env.root, "elevate.cmd"), filepath.Clean(env.symlink), filepath.Join(env.root, "v"+version)))
-		ok, err = elevatedRun("mklink", "/D", filepath.Clean(env.symlink), filepath.Join(env.root, "v"+version))
+		ok, err = elevatedRun("mklink", "/J", filepath.Clean(env.symlink), filepath.Join(env.root, "v"+version))
 		if err != nil {
 			if strings.Contains(err.Error(), "not have sufficient privilege") || strings.Contains(strings.ToLower(err.Error()), "access is denied") {
 				ok, err = elevatedRun("mklink", "/D", filepath.Clean(env.symlink), filepath.Join(env.root, "v"+version))
